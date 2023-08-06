2e007c9bec582d5cfe53f56a18ca2299931526a8
netrivas <dcobos00@gmail.com>
2023-08-06 22:07:23 +0000

    david branch

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 document.body.style.backgroundColor = 'black'
 let canvas = document.querySelector('canvas')
-let renderingContext = canvas.getContext('2d')
+let ctx = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-renderingContext.fillStyle = 'white'
-renderingContext.fillRect(0, 0, canvas.width, canvas.height)
+ctx.fillStyle = 'white'
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+let y = 100
+
+const animate = () => {
+    window.requestAnimationFrame(animate)
+    ctx.fillStyle = 'white'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.fillStyle = 'red'
+    ctx.fillRect(100, y, 100, 100)
+    y++
+}
+animate()
